503b96e37f33c25e4818fe366a6edf438561b5fd
Vladimir <x55aah@gmail.com>
2017-12-15 13:43:07 +0200

    Rollback

--- anthill/manifests/puppetdb.pp
+++ anthill/manifests/puppetdb.pp
@@ -3,14 +3,14 @@ class anthill::puppetdb {
 
   # Configure puppetdb and its underlying database
   class { puppetdb:
-    listen_address => "127.0.0.1"
+    listen_address => $::fqdn
   }
 
   Package['apt-transport-https'] -> Class[puppetdb]
 
   # Configure the Puppet master to use puppetdb
   class { puppetdb::master::config:
-    puppetdb_server => "127.0.0.1"
+    puppetdb_server => $::fqdn
   }
 
 }
